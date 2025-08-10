fe304097b246e437756fd1bab4ae62e58249743d
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-08-10 21:24:44 +0300

    feat(tg): create telegram login button component

---
+++ resources/js/components/login/TelegramLoginButton.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+type Props = {
+    route: string;
+    botName: string;
+};
+
+export default function TelegramLoginButton({ route, botName }: Props) {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://telegram.org/js/telegram-widget.js';
+        script.async = true;
+        script.setAttribute('data-telegram-login', botName);
+        script.setAttribute('data-size', 'large');
+        script.setAttribute('data-userpic', 'false');
+        script.setAttribute('data-auth-url', route);
+        script.setAttribute('data-request-access', 'write');
+        document.getElementById('telegram-login')?.appendChild(script);
+    });
+
+    return <div id="telegram-login"></div>;
+}
